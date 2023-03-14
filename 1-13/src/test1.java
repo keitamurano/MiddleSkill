9bf4f5dd435f5de323fbffb1a33e17271f307f23
keitamurano <7banters7@gmail.com>
2023-03-15 01:42:53 +0900

    1-13課題提出

---
+++ 1-13/src/test1.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class test1 {
+	public static void main(String[] args) {
+		//改行
+		System.out.println("問１");
+		//問題1:下記の条件を使ってfor文と条件文を組み合わせ結果が同じになるよう出力しなさい。for文ネスト3回（3回forを書くこと）
+		//strArray1でfor文を回し3文字目(k)以降のものをstrArray2に格納する(1個目のfor文)
+		//strArray2でfor文を回しkとaとoの物だけでstrArray3に格納する(2個目のfor文)
+		//strArray3でfor文を回し「kakao」となるようにstrArray4に格納しstrArray4を出力する。(3個目のfor文)
+		List<String> strArray1 = new ArrayList(Arrays.asList("f", "u", "k", "u", "d", "a", "k" , "a" , "n" , "e" , "k" , "o" ));
+		List<String> strArray2 = new ArrayList<String>();
+		List<String> strArray3 = new ArrayList<String>();
+		List<String> strArray4 = new ArrayList<String>();
+
+		loop : for(int a = 2; a < strArray1.size(); a++) {
+			strArray2.add(strArray1.get(a));
+			
+			for(int b = 0; b < strArray2.size(); b++) {
+				if(strArray2.get(b).equals("k") || strArray2.get(b).equals("a") || strArray2.get(b).equals("o")) {
+					strArray3.add(strArray2.get(b));
+				}
+			
+				for(int c = 0; c < strArray3.size(); c++) {
+					if(strArray3.get(c).equals("k") || strArray3.get(c).equals("a") || strArray3.get(c).equals("o")) {
+						strArray4.add("k");
+						strArray4.add("a");
+						strArray4.add("k");
+						strArray4.add("a");
+						strArray4.add("o");
+						break loop;
+						
+						}
+					//						System.out.println(strArray4);
+				}
+			}
+		}
+		System.out.println(strArray4);
+	}
+
+}
+
